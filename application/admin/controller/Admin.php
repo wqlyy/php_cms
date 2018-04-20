59d4b1535522ea58f93c6737ffb5623c6571f70b
wangqiang <wangqiangwen@163.com>
2018-04-20 11:33:32 +0800

    模板分离

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -3,8 +3,13 @@ namespace app\admin\controller;
 use think\Controller;
 class Admin extends Controller
 {
-    public function index()
-    {
+    public function index(){
         return view('list');
+    }
+    public function add(){
+    	return view('add');
+    }
+    public function edit(){
+    	return view('edit');
     }
 }
